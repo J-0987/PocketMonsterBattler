4ce70be0fd1032fb6aaa24f5d4026da494b630ce
Jasmine J <146461479+J-0987@users.noreply.github.com>
2024-03-26 13:55:26 -0400

    pseudocode

---
+++ assets/js/pseudocode.js
@@ -0,0 +1,74 @@
+//MAIN 
+
+//Upon page load
+    // header 
+    // Two empty cards 
+    // Start button 
+    //Tumblr feed 
+
+
+// Battling system
+        // When startBtn pressed : fetch name, type, and image - fetch command
+        //display cards to be played - random selection for both players - function to display card
+        // circular arrow button for option to switch pokemon - add eventlistener
+
+        // fight button to start battle
+            //hide switch pokemon button (bryan to share code)
+            //compare both pokemon types based on conditional arguments
+            // display results on table below fight cards
+            //display  W L T
+            //results stored in local storage (user pokemon, opponent pokemon, result, fight count of pokemon) - battleResults
+        
+        // TOADD to WF - continue playing button
+            // Clear data on top for win/lose/tie
+            // current user pokemon remains unless changed
+
+  
+        // Tumblr Feed
+            // Process to intialize API
+                //Reg app on Tumblr
+                //Set up OAuth
+                // fetch and sort fetch
+                // TODO - info filter "blog data, user data"
+                // TODO - automated passive process in background - refresh 
+                // functions
+                    // getAPI
+
+
+// RESULTS PAGE
+    // Display 
+        //6 top used cards, Best battler plus 5
+        //total wins, ties, and losses
+        //log of previous plays
+
+
+// STYLING
+    //Header 
+        //dayjs
+        //dark/light mode
+        // page title
+        //pikachu icon
+        
+
+    // battleField
+        //Row 1   
+         // 3 columns
+            //col 1 - user card
+            //col 2- VS box, fight button
+            //col 3- opponent card
+
+        //Row 2
+            // refresh button
+        
+    // scoreTabulation
+            //table for scores
+            //full stats button 
+        
+    //tumblrFeed r
+            //Tumblr feed 
+    
+    //fullStats
+        //
+
+
+
