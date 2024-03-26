b8a9ded1bb01ee3dc59b70fa2ca3feb7ce37d727
Jasmine J <146461479+J-0987@users.noreply.github.com>
2024-03-26 18:11:05 -0400

    psuedoCode, html for results page, and styling start for index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [".//**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
+
+
 }
+
+// Pokemon colours
+    //Pokémon Dark Blue: Hex: #3c5aa6, RGB: 60, 90, 166
+    // Pokémon Yellow Shadow: Hex: #c7a008 RGB: 199, 160, 8
+    // Pokémon Light Blue: Hex: #2a75bb  RGB: 42, 117, 187
+    // Pokémon Yellow : Hex: #ffcb05 RGB: 255, 203, 5
+
+    
+
+
+
+
 
