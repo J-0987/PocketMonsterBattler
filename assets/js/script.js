5dd64fcc4a544f6f7ce8587a59f804bbe77e32fd
Jasmine J <146461479+J-0987@users.noreply.github.com>
2024-03-27 21:50:26 -0400

    continued styling dark theme

---
+++ assets/js/script.js
@@ -0,0 +1,2 @@
+const today = dayjs();
+$('#day').text(today.format('ddd/MM/D/YY, h:mm:ss a'));
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,35 @@
 module.exports = {
   content: [".//**/*.{html,js}"],
   theme: {
-    extend: {},
+
+    extend: {
+      colors: {
+        themeColor: {
+          pokeblue: 'var(--pokeblue)',
+          pokedrkblue: 'var(--pokedrkblue)',
+          pokeyellow: 'var(--pokeyellow)',
+          pokeyellowshadow: 'var(--pokeyellowshadow)',
+
+        }
+      }
+
+    },
   },
   plugins: [],
-  darkMode: "class",
+
 
 
 }
 
-// Pokemon colours
-    //Pokémon Dark Blue: Hex: #3c5aa6, RGB: 60, 90, 166
-    // Pokémon Yellow Shadow: Hex: #c7a008 RGB: 199, 160, 8
-    // Pokémon Light Blue: Hex: #2a75bb  RGB: 42, 117, 187
-    // Pokémon Yellow : Hex: #ffcb05 RGB: 255, 203, 5
 
-    
+
+// Pokemon colours
+//Pokémon Dark Blue: Hex: #3c5aa6, RGB: 60, 90, 166
+// Pokémon Yellow Shadow: Hex: #c7a008 RGB: 199, 160, 8
+// Pokémon Light Blue: Hex: #2a75bb  RGB: 42, 117, 187
+// Pokémon Yellow : Hex: #ffcb05 RGB: 255, 203, 5
+
+
 
 
 
